a3cdbb8c61311fdb442d43827364f7cd0e106adc
Stallion <Stallion@LAPTOP-OOHQ7RKF>
2021-07-06 21:18:29 +0530

    Payment Class Created

---
+++ src/main/java/com/abc/healthcare/DcxhealthcenterApplication.java
@@ -0,0 +1,13 @@
+package com.abc.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcxhealthcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcxhealthcenterApplication.class, args);
+	}
+
+}
