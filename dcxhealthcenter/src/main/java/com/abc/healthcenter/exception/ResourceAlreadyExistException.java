34d98608a4e28349b9ea717d479fbb415777511b
NAGA SRI HARSHA <NAGA SRI HARSHA@DESKTOP-VKPVT55>
2021-07-07 12:17:50 +0530

    added resourcealreadyexistexception

--- dcxhealthcenter/src/main/java/com/abc/healthcenter/exception/ResourceAlreadyExistException.java
+++ dcxhealthcenter/src/main/java/com/abc/healthcenter/exception/ResourceAlreadyExistException.java
@@ -3,6 +3,11 @@ package com.abc.healthcenter.exception;
 
 public class ResourceAlreadyExistException extends RuntimeException {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ResourceAlreadyExistException(String message) {
 		super(message);
 	}
